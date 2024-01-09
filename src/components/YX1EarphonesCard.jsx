f6326dfe160e0c48d43555548caaa8d932b7c05e
htrung11120 <143675045+htrung11120@users.noreply.github.com>
2024-01-09 02:21:33 -0500

    Update YX1EarphonesCard.jsx

--- src/components/YX1EarphonesCard.jsx
+++ src/components/YX1EarphonesCard.jsx
@@ -5,7 +5,7 @@ export default function YX1EarphonesCard() {
 
   return (
     <div className="mt-6 flex flex-col gap-6 sm:mx-10 sm:flex-row lg:mx-[165px]">
-      <div className="mx-6 h-52 rounded-lg bg-[url(/src/assets/home/mobile/image-earphones-yx1.jpg)] bg-contain sm:mx-0  sm:h-[320px] sm:flex-grow sm:bg-[url(/src/assets/home/tablet/image-earphones-yx1.jpg)] sm:bg-cover lg:w-[540px] lg:grow lg:bg-[url(/src/assets/home/desktop/image-earphones-yx1.jpg)]"></div>
+      <div className="mx-6 h-52 rounded-lg bg-[url(/assets/home/mobile/image-earphones-yx1.jpg)] bg-contain sm:mx-0  sm:h-[320px] sm:flex-grow sm:bg-[url(/assets/home/tablet/image-earphones-yx1.jpg)] sm:bg-cover lg:w-[540px] lg:grow lg:bg-[url(/assets/home/desktop/image-earphones-yx1.jpg)]"></div>
       <div className="mx-6 flex h-52 flex-col justify-center gap-8 rounded-lg bg-[#F1F1F1] pl-6 sm:mx-0   sm:h-[320px] sm:w-[340px]  lg:w-[540px] lg:grow">
         <p className="text-[28px] font-bold uppercase tracking-[2px] ">
           YX1 EARPHONES

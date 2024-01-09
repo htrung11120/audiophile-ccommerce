774847dece234d3d3014c9039e98dc040eba965d
htrung11120 <143675045+htrung11120@users.noreply.github.com>
2024-01-09 02:22:47 -0500

    Update ZX7SpeakerCard.jsx

--- src/components/ZX7SpeakerCard.jsx
+++ src/components/ZX7SpeakerCard.jsx
@@ -4,7 +4,7 @@ export default function ZX7Speaker() {
   const navigate = useNavigate();
 
   return (
-    <div className="mx-6 flex h-[19rem] flex-col justify-center  gap-8 rounded-lg bg-[url(/src/assets/home/mobile/image-speaker-zx7.jpg)] bg-cover pl-6 sm:mx-10 sm:bg-[url(/src/assets/home/tablet/image-speaker-zx7.jpg)] sm:bg-cover  lg:mx-[165px] lg:bg-[url(/src/assets/home/desktop/image-speaker-zx7.jpg)]">
+    <div className="mx-6 flex h-[19rem] flex-col justify-center  gap-8 rounded-lg bg-[url(/assets/home/mobile/image-speaker-zx7.jpg)] bg-cover pl-6 sm:mx-10 sm:bg-[url(/assets/home/tablet/image-speaker-zx7.jpg)] sm:bg-cover  lg:mx-[165px] lg:bg-[url(/assets/home/desktop/image-speaker-zx7.jpg)]">
       <p className=" text-[28px] font-bold uppercase tracking-[2px]">
         ZX7 Speaker
       </p>
